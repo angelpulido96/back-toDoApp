ff63ef12c7187097d9518652699509175ed14e75
apulido <angel@dicalzeus.com>
2023-04-19 15:22:04 -0700

    Added imgur api

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const routes = require('./network/routes')
 const connectionDB = require('./db')
 
-app.use(express.json())
+app.use(express.json({ limit: "200mb" }))
 app.use(cors());
 app.use(express.urlencoded({ extended: false }))
 
